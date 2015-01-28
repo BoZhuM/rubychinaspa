1513072315b9c5e203f22e21962fb873089cfcef
suffering <zhuboliu@gmail.com>
2015-01-28 21:58:51 +0800

    修复 atUser Directive 将正常外链变更的bug

--- js/directives.js
+++ js/directives.js
@@ -2,18 +2,19 @@
 var rubychinaDirectives = angular.module('rubychinaDirectives', []);
 
 //这条directive用于将topic.reply_body中的.at_user的链接原始替换为spa项目支持的链接.
+//个人的原生项目, 自己写api不需要这样做.
 rubychinaDirectives.directive('atUser', function() {
     return {
         restrict: 'A',
         link: function (scope, element, attrs) {
             var pattern = /users/i
-            element.on("mouseenter", function(){
-                angular.forEach(element.find("a"), function(e){
-                   var _el = angular.element(e);
-                   var _href = _el.attr("href");
-                   if (_href.search(pattern) == -1)
-                       _el.attr("href", "#/users" + _href)
-                })
+            element.on("click", function(e){
+              // atUser directive的下级元素, 点击时判断是否@user模式, 若是, 则替换为本spa项目的链接
+              var _el      = angular.element(e.toElement);
+              var _href    = _el.attr("href");
+              var at_title = _el.attr('title');
+              if (_href.search(pattern) == -1 && at_title && at_title.search(/^@/i) == 0)
+                _el.attr("href", "#/users" + _href);
             })
         }
     }
